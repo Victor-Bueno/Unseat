dcb6102d3c5e8649aa5c3f5b04190d5aeec98900
Victor <victor.bueno0720@gmail.com>
2021-03-09 19:59:29 -0300

    Implemented experience bar component

---
+++ unseat/src/components/ExperienceBar.tsx
@@ -0,0 +1,14 @@
+const ExperienceBar = () => {
+    return (
+        <header className="experience-bar-container">
+            <span>0 xp</span>
+            <div>
+                <div style={{ width: "50%" }} />
+                <span style={{ left: "50%" }} className="current-experience">300 xp</span>
+            </div>
+            <span>600 xp</span>
+        </header>
+    );
+}
+
+export default ExperienceBar;
