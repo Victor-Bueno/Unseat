3caf1a7692e856f56ca5c963b0afc9918f976fc7
Victor <victor.bueno0720@gmail.com>
2021-03-13 15:55:53 -0300

    Showing real data to completed challenges

--- unseat-next/src/components/CompletedChallenges.tsx
+++ unseat-next/src/components/CompletedChallenges.tsx
@@ -1,10 +1,16 @@
+import { useContext } from 'react';
+
 import styles from '../styles/components/CompletedChallenges.module.css';
 
+import ChallengesContext from '../contexts/ChallengesContext';
+
 const CompletedChallenges = () => {
+    const { completedChallenges } = useContext(ChallengesContext);
+
     return (
         <div className={styles.completedChallengesContainer}>
             <span>Desafios Completos</span>
-            <span>5</span>
+            <span>{completedChallenges}</span>
         </div>
     );
 }
